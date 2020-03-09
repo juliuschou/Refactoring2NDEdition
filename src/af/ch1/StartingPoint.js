b4d78e67fa5ab4ed7b8aa92cf78ba38ead1044a0
wanzi chou <juliuschou@gmail.com>
2020-03-09 18:02:21 +0800

    change param name of volumeCreditFor

--- src/af/ch1/StartingPoint.js
+++ src/af/ch1/StartingPoint.js
@@ -50,12 +50,12 @@ function statement(invoice, plays) {
     return result;
 }
 
-function volumeCreditFor(perf) {
+function volumeCreditFor(aPerformance) {
     let volumeCredits = 0;
 
-    volumeCredits += Math.max(perf.audience - 30, 0);
+    volumeCredits += Math.max(aPerformance.audience - 30, 0);
 
-    if ('comedy' === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+    if ('comedy' === playFor(aPerformance).type) volumeCredits += Math.floor(aPerformance.audience / 5);
 
 }
 
