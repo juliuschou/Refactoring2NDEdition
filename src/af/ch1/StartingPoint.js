ca07366eed1390594c75d2d07b9924e570e39369
周丸生(SamChou) <SamChou@chipbond.com.tw>
2020-03-12 09:57:21 +0800

    rename para name of volumeCreditsFor

--- src/af/ch1/StartingPoint.js
+++ src/af/ch1/StartingPoint.js
@@ -49,10 +49,10 @@ function statement(invoice, plays) {
     return result;
 }
 
-function volumeCreditsFor(perf){
+function volumeCreditsFor(aPerformance){
     let volumeCredits = 0;
-    volumeCredits += Math.max(perf.audience - 30, 0);
-    if ('comedy' === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+    volumeCredits += Math.max(aPerformance.audience - 30, 0);
+    if ('comedy' === playFor(aPerformance).type) volumeCredits += Math.floor(aPerformance.audience / 5);
 
     return volumeCredits;
 }
