be0539cfa9afd98f64608fbf2eeb826e7f7daad2
周丸生(SamChou) <SamChou@chipbond.com.tw>
2020-03-13 21:43:57 +0800

    Extract Function (106) to the code that makes up the second phase

--- src/af/ch1/StartingPoint.js
+++ src/af/ch1/StartingPoint.js
@@ -1,4 +1,4 @@
-// node ./node_modules/.bin/babel-node src/xx.js
+// node ./dist/af/ch1/StartingPoint.js
 // ./node_modules/.bin/babel src --out-dir dist
 
 let plays = {
@@ -29,6 +29,10 @@ let rlt = statement(invoices[0], plays);
 console.log(rlt);
 
 function statement(invoice, plays) {
+    return renderPlainText(invoice, plays);
+}
+
+function renderPlainText(invoice, plays) {
     let result = `Statement for ${invoice.customer}\n`;
  
     for (let perf of invoice['performances']) {
@@ -41,62 +45,66 @@ function statement(invoice, plays) {
     result += `You earned ${volumeCredits} credits\n`;
     return result;
 
-}
-
-
-function totalAmount(invoice){
-    let totalAmount = 0;
-    for (let perf of invoice['performances']) {
-        totalAmount += amountFor(perf);
+    function totalAmount(){
+        let totalAmount = 0;
+        for (let perf of invoice['performances']) {
+            totalAmount += amountFor(perf);
+        }
+        return totalAmount;    
     }
-    return totalAmount;    
-}
-
-function totalVolumeCredits(invoice){
-    let result = 0;
-    for (let perf of invoice['performances']) {
-        result += volumeCreditsFor(perf);
+    
+    function totalVolumeCredits(){
+        let result = 0;
+        for (let perf of invoice['performances']) {
+            result += volumeCreditsFor(perf);
+        }
+        return result;
     }
-    return result;
-}
 
-function volumeCreditsFor(aPerformance){
-    let result = 0;
-    result += Math.max(aPerformance.audience - 30, 0);
-    if ('comedy' === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
-
-    return result;
-}
-
-function usd(aNumber){
-   //const format = new Intl.NumberFormat("en-­US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format;
-
-    return new Intl.NumberFormat('en-IN', { maximumSignificantDigits: 3 }).format(aNumber);
-}
-
-function amountFor(aPerformance) {
-    let result = 0;
-    const play = playFor(aPerformance);
-    switch (play.type) {
-        case "tragedy":
-            result = 40000;
-            if (aPerformance.audience > 30) {
-                result += 1000 * (aPerformance.audience - 30);
-            }
-            break;
-        case "comedy":
-            result = 30000;
-            if (aPerformance.audience > 20) {
-                result += 10000 + 500 * (aPerformance.audience - 20);
-            }
-            result += 300 * aPerformance.audience;
-            break;
-        default:
-            throw new Error(`unknown type: ${play.type}`);
+    function volumeCreditsFor(aPerformance){
+        let result = 0;
+        result += Math.max(aPerformance.audience - 30, 0);
+        if ('comedy' === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
+    
+        return result;
     }
-    return result;
+    
+    function usd(aNumber){
+       //const format = new Intl.NumberFormat("en-­US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format;
+    
+        return new Intl.NumberFormat('en-IN', { maximumSignificantDigits: 3 }).format(aNumber);
+    }
+    
+    function amountFor(aPerformance) {
+        let result = 0;
+        const play = playFor(aPerformance);
+        switch (play.type) {
+            case "tragedy":
+                result = 40000;
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30);
+                }
+                break;
+            case "comedy":
+                result = 30000;
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20);
+                }
+                result += 300 * aPerformance.audience;
+                break;
+            default:
+                throw new Error(`unknown type: ${play.type}`);
+        }
+        return result;
+    }
+    
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID];
+    }
+
 }
 
-function playFor(aPerformance) {
-    return plays[aPerformance.playID];
-}
+
+
+
+
