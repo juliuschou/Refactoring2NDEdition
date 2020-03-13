7317d595535c2686bb27e98c8e4965124ac1018f
周丸生(SamChou) <SamChou@chipbond.com.tw>
2020-03-13 08:21:44 +0800

    Slide Statements to bring totalAmount next to accu
    Slide Statements (223) to bring the initializing code next to the accumulation

--- src/af/ch1/StartingPoint.js
+++ src/af/ch1/StartingPoint.js
@@ -29,7 +29,6 @@ let rlt = statement(invoices[0], plays);
 console.log(rlt);
 
 function statement(invoice, plays) {
-    let totalAmount = 0;
     let result = `Statement for ${invoice.customer}\n`;
  
     for (let perf of invoice['performances']) {
@@ -39,6 +38,7 @@ function statement(invoice, plays) {
         result += ` ${play.name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
     }
  
+    let totalAmount = 0;
     for (let perf of invoice['performances']) {
         totalAmount += amountFor(perf);
     }
