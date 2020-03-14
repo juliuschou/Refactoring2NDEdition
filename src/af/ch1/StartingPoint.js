d65ea6aa2bbdca6a6599270ce5f46a664580f688
周丸生(SamChou) <SamChou@chipbond.com.tw>
2020-03-14 20:50:33 +0800

    take the customer & add it to the intermediate obj

--- src/af/ch1/StartingPoint.js
+++ src/af/ch1/StartingPoint.js
@@ -30,11 +30,12 @@ console.log(rlt);
 
 function statement(invoice, plays) {
     const statementData = {};
+    statementData.customer = invoice.customer;
     return renderPlainText(statementData, invoice, plays);
 }
 
 function renderPlainText(data, invoice, plays) {
-    let result = `Statement for ${invoice.customer}\n`;
+    let result = `Statement for ${data.customer}\n`;
  
     for (let perf of invoice['performances']) {
         result += ` ${playFor(perf).name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
