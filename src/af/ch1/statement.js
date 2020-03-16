df75aaca6ae93083c74b88a81330495e508f4283
周丸生(SamChou) <SamChou@chipbond.com.tw>
2020-03-16 18:15:04 +0800

    write an HTML version

--- src/af/ch1/statement.js
+++ src/af/ch1/statement.js
@@ -1,6 +1,6 @@
 // node ./dist/af/ch1/StartingPoint.js
 // ./node_modules/.bin/babel src --out-dir dist
-
+// node ./dist/af/ch1/statement.js
 import createStatementData from './createStatementData';
 
 let plays = {
@@ -30,6 +30,10 @@ let invoices = [{
 let rlt = statement(invoices[0], plays);
 console.log(rlt);
 
+let rltHtml = htmlStatement(invoices[0], plays);
+console.log(rltHtml);
+
+
 function statement(invoice, plays) {
     return renderPlainText(createStatementData(invoice, plays));
 }
@@ -43,12 +47,30 @@ function renderPlainText(data) {
     result += `Amount owed is ${usd(data.totalAmount / 100)}\n`;
     result += `You earned ${data.totalVolumeCredits} credits\n`;
     return result;
-   
-    function usd(aNumber){
-       //const format = new Intl.NumberFormat("en-­US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format;
-    
-        return new Intl.NumberFormat('en-IN', { maximumSignificantDigits: 3 }).format(aNumber);
-    }  
         
 }
 
+function usd(aNumber){
+    //const format = new Intl.NumberFormat("en-­US", { style: "currency", currency: "USD", minimumFractionDigits: 2 }).format;
+ 
+     return new Intl.NumberFormat('en-IN', { maximumSignificantDigits: 3 }).format(aNumber);
+ }  
+ 
+function htmlStatement(invoice, plays) {
+    return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHtml(data) {
+    let result = `<h1>Statement for ${data.customer}</h1>\n`;
+    result += "<table>\n";
+    result += "<tr><th>play</th><th>seats</th><th>cost</th></tr>";
+   
+    for (let perf of data.performances) {
+        result += ` <tr><td>${perf.play.name}</td><td>${perf.audience}</td>`;
+        result += `<td>${usd(perf.amount)}</td></tr>\n`;
+    }
+    result += "</table>\n";
+    result += `<p>Amount owed is <em>${usd(data.totalAmount)}</em></p>\n`;
+    result += `<p>You earned <em>${data.totalVolumeCredits}</em> credits</p>\n`;
+    return result;
+}
