24558b897dfba9758db009cf8ba1ce0744937203
周丸生(SamChou) <SamChou@chipbond.com.tw>
2020-03-17 20:17:49 +0800

    move amoutFor() to PerformanceCalculator

--- src/af/ch1/createStatementData.js
+++ src/af/ch1/createStatementData.js
@@ -72,6 +72,30 @@ class PerformanceCalculator {
         this.performance = aPerformance;
         this.play = aPlay;
     }
+
+    get amount(aPerformance) {
+        let result = 0;
+        switch (aPerformance.play.type) {
+            case "tragedy":
+                result = 40000;
+                if (aPerformance.audience > 30) {
+                    result += 1000 * (aPerformance.audience - 30);
+                }
+                break;
+            case "comedy":
+                result = 30000;
+                if (aPerformance.audience > 20) {
+                    result += 10000 + 500 * (aPerformance.audience - 20);
+                }
+                result += 300 * aPerformance.audience;
+                break;
+            default:
+                throw new Error(`unknown type: ${play.type}`);
+        }
+        return result;
+    }
+
+
 }
 
 /*
