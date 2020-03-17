96fad5bf7ac5579cf724ae06091452e82e7a5406
周丸生(SamChou) <SamChou@chipbond.com.tw>
2020-03-17 22:49:14 +0800

    I'm as paranoid as author

--- src/af/ch1/createStatementData.js
+++ src/af/ch1/createStatementData.js
@@ -57,11 +57,7 @@ class PerformanceCalculator {
         let result = 0;
         switch (this.play.type) {
             case "tragedy":
-                result = 40000;
-                if (this.performance.audience > 30) {
-                    result += 1000 * (this.performance.audience - 30);
-                }
-                break;
+                throw 'bad thing';
             case "comedy":
                 result = 30000;
                 if (this.performance.audience > 20) {
