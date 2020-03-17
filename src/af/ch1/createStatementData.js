50cf218da8dd434b73ebcbe5ec8fa8b2adc8dce9
周丸生(SamChou) <SamChou@chipbond.com.tw>
2020-03-17 22:43:59 +0800

    Replace Conditional with Polymorphism- calc amount
    I can now move on to Replace Conditional with Polymorphism (272). I start with the calculation of the amount for tragedies.

--- src/af/ch1/createStatementData.js
+++ src/af/ch1/createStatementData.js
@@ -87,6 +87,13 @@ class PerformanceCalculator {
 }
 
 class TragedyCalculator extends PerformanceCalculator {
+    get amount() {
+        let result = 40000;
+        if (this.performance.audience > 30) {
+            result += 1000 * (this.performance.audience - 30);
+        }
+        return result;
+    }    
 }
 
 class ComedyCalculator extends PerformanceCalculator {
