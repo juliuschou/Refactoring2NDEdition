0b13ddd7663b541214034a60902370a26f4e4d78
周丸生(SamChou) <SamChou@chipbond.com.tw>
2020-03-17 21:34:34 +0800

    use Inline Function to call cactor.amount directly
    use Inline Function (115) to call the new function directly

--- src/af/ch1/createStatementData.js
+++ src/af/ch1/createStatementData.js
@@ -12,7 +12,7 @@ export default function createStatementData (invoice, plays) {
         const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
         const result = Object.assign({}, aPerformance);
         result.play = calculator.play;
-        result.amount = amountFor(result);
+        result.amount = calculator.amount;
         result.volumeCredits = volumeCreditsFor(result);
         return result;
     }     
