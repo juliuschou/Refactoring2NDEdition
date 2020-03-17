046ca4aca7b14ed6c65b84549cc5fb809b0ea55a
周丸生(SamChou) <SamChou@chipbond.com.tw>
2020-03-17 21:54:01 +0800

    take original fun & turn it into a delegating fun
    (volumeCredits) I take the original function and turn it into a
    delegating function so it calls the new function.

--- src/af/ch1/createStatementData.js
+++ src/af/ch1/createStatementData.js
@@ -22,11 +22,7 @@ export default function createStatementData (invoice, plays) {
     }    
     
     function volumeCreditsFor(aPerformance){
-        let result = 0;
-        result += Math.max(aPerformance.audience - 30, 0);
-        if ('comedy' === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-    
-        return result;
+        return new PerformanceCalculator(aPerformance, playFor(aPerformance)).volumeCredits;
     }
     
     /*
