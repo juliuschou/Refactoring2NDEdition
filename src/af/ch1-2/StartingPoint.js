3b2a39f62604caafaf90fe9dcf9ad52df6b804fd
周丸生(SamChou) <SamChou@chipbond.com.tw>
2020-03-20 19:57:52 +0800

    call to the target function

--- src/af/ch1-2/StartingPoint.js
+++ src/af/ch1-2/StartingPoint.js
@@ -37,24 +37,8 @@ function statement(invoice, plays) {
 
     for (let perf of invoice['performances']) {
         const play = plays[perf.playID];
-        let thisAmount = 0;
-        switch (play.type) {
-            case 'tragedy':
-                thisAmount = 40000;
-                if (perf.audience > 30) {
-                    thisAmount += 1000 * (perf.audience - 30);
-                }
-                break;
-            case 'comedy':
-                thisAmount = 30000;
-                if (perf.audience > 20) {
-                    thisAmount += 10000 + 500 * (perf.audience - 20);
-                }
-                thisAmount += 300 * perf.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${play.type}`);
-        }
+        let thisAmount = amountFor(perf, play);
+       
         // add volume credits
         volumeCredits += Math.max(perf.audience - 30, 0);
         // add extra credit for every ten comedy attendees
