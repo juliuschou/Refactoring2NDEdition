ed2e3474dcf74c046ee78e29a8e9becff3b1d165
周丸生(SamChou) <SamChou@chipbond.com.tw>
2020-03-20 19:44:40 +0800

    copy the extracted code from the src fun.

--- src/af/ch1-2/StartingPoint.js
+++ src/af/ch1-2/StartingPoint.js
@@ -70,7 +70,24 @@ function statement(invoice, plays) {
     return result;
 
     function amountFor(){
-        
+        switch (play.type) {
+            case 'tragedy':
+                thisAmount = 40000;
+                if (perf.audience > 30) {
+                    thisAmount += 1000 * (perf.audience - 30);
+                }
+                break;
+            case 'comedy':
+                thisAmount = 30000;
+                if (perf.audience > 20) {
+                    thisAmount += 10000 + 500 * (perf.audience - 20);
+                }
+                thisAmount += 300 * perf.audience;
+                break;
+            default:
+                throw new Error(`unknown type: ${play.type}`);
+        }
+       
     }
 
 }
