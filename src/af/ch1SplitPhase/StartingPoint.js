fe6941501b3fec1034c0744ae629f9659630202c
周丸生(SamChou) <SamChou@chipbond.com.tw>
2020-03-21 20:38:36 +0800

    start a split phase by applying extract fun.
    I start a Split Phase (154) by applying Extract Function (106) to the code that makes up
    the second phase. In this case, that’s the statement printing code, which is in fact the
    entire content of statement. This, together with all the nested functions, goes into its
    own toplevel
    function which I call renderPlainText.

--- src/af/ch1SplitPhase/StartingPoint.js
+++ src/af/ch1SplitPhase/StartingPoint.js
@@ -1,5 +1,5 @@
 // ./node_modules/.bin/babel src --out-dir dist
-// node ./dist/af/ch1-2/StartingPoint.js
+//  node ./dist/af/ch1SplitPhase/StartingPoint.js
 
 let plays = {
     hamlet: { name: 'Hamlet', type: 'tragedy' },
@@ -29,6 +29,10 @@ let rlt = statement(invoices[0], plays);
 console.log(rlt);
 
 function statement(invoice, plays) {
+    return renderPlainText(invoice, plays);
+}
+
+function renderPlainText(invoice, plays) {
     let result = `Statement for ${invoice.customer}\n`;
 
     for (let perf of invoice['performances']) {
