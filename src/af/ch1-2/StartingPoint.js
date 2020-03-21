00c3b988e439493281aabbb383bee85cf12f3b0b
周丸生(SamChou) <SamChou@chipbond.com.tw>
2020-03-21 11:17:07 +0800

    change amountFor declaration

--- src/af/ch1-2/StartingPoint.js
+++ src/af/ch1-2/StartingPoint.js
@@ -56,7 +56,7 @@ function statement(invoice, plays) {
         return plays[aPerformance.playID];
     }            
 
-    function amountFor(aPerformance, play){
+    function amountFor(aPerformance){
         let result = 0;
         
         switch (playFor(aPerformance).type) {
