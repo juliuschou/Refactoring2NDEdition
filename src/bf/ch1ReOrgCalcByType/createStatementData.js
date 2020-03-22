962268b9f09b2faaa8ae3a16cc8768c53d31baaf
周丸生(SamChou) <SamChou@chipbond.com.tw>
2020-03-22 14:43:53 +0800

    mv amoutnFor: turn src fun. into a delegating fun

--- src/bf/ch1ReOrgCalcByType/createStatementData.js
+++ src/bf/ch1ReOrgCalcByType/createStatementData.js
@@ -72,25 +72,6 @@ export default function createStatementData(invoice, plays) {
     }  
 
     function amountFor(aPerformance){
-        let result = 0;
-        
-        switch (aPerformance.play.type) {
-            case 'tragedy':
-                result = 40000;
-                if (aPerformance.audience > 30) {
-                    result += 1000 * (aPerformance.audience - 30);
-                }
-                break;
-            case 'comedy':
-                result = 30000;
-                if (aPerformance.audience > 20) {
-                    result += 10000 + 500 * (aPerformance.audience - 20);
-                }
-                result += 300 * aPerformance.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${playFor(aPerformance).type}`);
-        }
-       return result;
-    }    
+        return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
+    }
 }
