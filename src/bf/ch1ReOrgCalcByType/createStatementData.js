5722c1304a41436623c4cb1009a481df720f211a
周丸生(SamChou) <SamChou@chipbond.com.tw>
2020-03-22 15:05:19 +0800

    create subclasses & select which one to return.
    I can create subclasses of the performance calculator and get
    the creation function to select which one to return.

--- src/bf/ch1ReOrgCalcByType/createStatementData.js
+++ src/bf/ch1ReOrgCalcByType/createStatementData.js
@@ -17,8 +17,22 @@ export default function createStatementData(invoice, plays) {
     }
 
     function createPerformanceCalculator(aPerformance, aPlay) {
-        return new PerformanceCalculator(aPerformance, aPlay);
+        switch(aPlay.type) {
+            case "tragedy": return new TragedyCalculator(aPerformance, aPlay);
+            case "comedy" : return new ComedyCalculator(aPerformance, aPlay);
+            default:      
+                throw new Error(`unknown type: ${aPlay.type}`);
+        }              
     }
+
+    class TragedyCalculator extends PerformanceCalculator {
+  
+    }
+    
+    class ComedyCalculator extends PerformanceCalculator {
+        
+    }    
+    
     class PerformanceCalculator {
         constructor(aPerformance, aPlay) {
             this.performance = aPerformance;
