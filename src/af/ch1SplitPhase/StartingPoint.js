946f47c37f20c7939ae40937d17c5e2cfc85c4a7
周丸生(SamChou) <SamChou@chipbond.com.tw>
2020-03-22 13:32:43 +0800

    mv 1st-phase code to its own file

--- src/af/ch1SplitPhase/StartingPoint.js
+++ src/af/ch1SplitPhase/StartingPoint.js
@@ -1,6 +1,8 @@
 // ./node_modules/.bin/babel src --out-dir dist
 //  node ./dist/af/ch1SplitPhase/StartingPoint.js
 
+import createStatementData from './createStatementData.js'
+
 let plays = {
     hamlet: { name: 'Hamlet', type: 'tragedy' },
     'as­like': { name: 'As You Like It', type: 'comedy' },
@@ -30,72 +32,6 @@ console.log(rlt);
 
 function statement(invoice, plays) {
     return renderPlainText(createStatementData(invoice, plays));
-}
-
-function createStatementData(invoice, plays) {
-    const statementData = {};
-    statementData.customer = invoice.customer;
-    statementData.performances = invoice.performances.map(enrichPerformance);
-    statementData.totalAmount = totalAmount(statementData);
-    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-    return statementData;
-
-    function enrichPerformance(aPerformance) {
-        const result = Object.assign({}, aPerformance);        
-        result.play = playFor(result);
-        result.amount = amountFor(result);
-        result.volumeCredits = volumeCreditsFor(result);
-        return result;
-    }
-
-    /*
-        https://developer.mozilla.org/zh-TW/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce
-        arr.reduce(callback[accumulator, currentValue, currentIndex, array], initialValue)
-     */
-
-    function totalAmount(data) {
-        return data.performances
-        .reduce((total, p) => total + p.amount, 0);
-    }
-
-    function totalVolumeCredits(data) {
-        return data.performances
-        .reduce((total, p) => total + p.volumeCredits, 0);
-    }     
-
-    function volumeCreditsFor(aPerformance){
-        let result = 0;
-        result += Math.max(aPerformance.audience - 30, 0);
-        if ('comedy' === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-        return result;
-    }          
-
-    function playFor(aPerformance) {
-        return plays[aPerformance.playID];
-    }  
-
-    function amountFor(aPerformance){
-        let result = 0;
-        
-        switch (aPerformance.play.type) {
-            case 'tragedy':
-                result = 40000;
-                if (aPerformance.audience > 30) {
-                    result += 1000 * (aPerformance.audience - 30);
-                }
-                break;
-            case 'comedy':
-                result = 30000;
-                if (aPerformance.audience > 20) {
-                    result += 10000 + 500 * (aPerformance.audience - 20);
-                }
-                result += 300 * aPerformance.audience;
-                break;
-            default:
-                throw new Error(`unknown type: ${playFor(aPerformance).type}`);
-        }
-       return result;
-    }    
 }
 
 function renderPlainText(data) {
