ceb8399a82304cb54d5dcbe9523ebf2f2c041861
周丸生(SamChou) <SamChou@chipbond.com.tw>
2020-03-22 13:28:38 +0800

    extract all the 1st-phase code into its own fun.
    extract all the 1st-phase code into its own funtion

--- src/af/ch1SplitPhase/StartingPoint.js
+++ src/af/ch1SplitPhase/StartingPoint.js
@@ -29,12 +29,16 @@ let rlt = statement(invoices[0], plays);
 console.log(rlt);
 
 function statement(invoice, plays) {
+    return renderPlainText(createStatementData(invoice, plays));
+}
+
+function createStatementData(invoice, plays) {
     const statementData = {};
     statementData.customer = invoice.customer;
     statementData.performances = invoice.performances.map(enrichPerformance);
     statementData.totalAmount = totalAmount(statementData);
     statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-    return renderPlainText(statementData, plays);
+    return statementData;
 
     function enrichPerformance(aPerformance) {
         const result = Object.assign({}, aPerformance);        
@@ -94,7 +98,7 @@ function statement(invoice, plays) {
     }    
 }
 
-function renderPlainText(data, plays) {
+function renderPlainText(data) {
     let result = `Statement for ${data.customer}\n`;
     for (let perf of data['performances']) {
         result += ` ${perf.play.name}: ${usd(perf.amount / 100)} (${perf.audience} seats)\n`;
