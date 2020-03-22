b82423dee33c70a467d6b72d5ae3103d668784bb
周丸生(SamChou) <SamChou@chipbond.com.tw>
2020-03-22 14:48:43 +0800

    use the Inline fun. to call the new fun

--- src/bf/ch1ReOrgCalcByType/createStatementData.js
+++ src/bf/ch1ReOrgCalcByType/createStatementData.js
@@ -10,7 +10,7 @@ export default function createStatementData(invoice, plays) {
         const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
         const result = Object.assign({}, aPerformance);        
         result.play = calculator.play;
-        result.amount = amountFor(result);
+        result.amount = calculator.amount;
         result.volumeCredits = volumeCreditsFor(result);
         return result;
     }
@@ -71,7 +71,4 @@ export default function createStatementData(invoice, plays) {
         return plays[aPerformance.playID];
     }  
 
-    function amountFor(aPerformance){
-        return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
-    }
 }
