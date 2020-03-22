154753b7c17e557e15968c9e6d53f099939b01b7
周丸生(SamChou) <SamChou@chipbond.com.tw>
2020-03-22 12:28:31 +0800

    add play for each perf. in inter*ate data

--- src/af/ch1SplitPhase/StartingPoint.js
+++ src/af/ch1SplitPhase/StartingPoint.js
@@ -33,16 +33,22 @@ function statement(invoice, plays) {
     statementData.customer = invoice.customer;
     statementData.performances = invoice.performances.map(enrichPerformance);
     return renderPlainText(statementData, plays);
-}
-function enrichPerformance(aPerformance) {
-    const result = Object.assign({}, aPerformance);
-    return result;
+
+    function enrichPerformance(aPerformance) {
+        const result = Object.assign({}, aPerformance);        
+        result.play = playFor(aPerformance);
+        return result;
+    }
+    
+    function playFor(aPerformance) {
+        return plays[aPerformance.playID];
+    }  
 }
 
 function renderPlainText(data, plays) {
     let result = `Statement for ${data.customer}\n`;
     for (let perf of data['performances']) {
-        result += ` ${playFor(perf).name}: ${usd(amountFor(perf, playFor(perf)) / 100)} (${perf.audience} seats)\n`;
+        result += ` ${perf.play.name}: ${usd(amountFor(perf) / 100)} (${perf.audience} seats)\n`;
     }
 
     result += `Amount owed is ${usd(totalAmount() / 100)}\n`;
@@ -52,7 +58,7 @@ function renderPlainText(data, plays) {
     function totalAmount(){
         let result = 0;
         for (let perf of data['performances']) {
-            result += amountFor(perf, playFor(perf));
+            result += amountFor(perf);
         }    
         return result;
     }
@@ -72,18 +78,14 @@ function renderPlainText(data, plays) {
     function volumeCreditsFor(aPerformance){
         let result = 0;
         result += Math.max(aPerformance.audience - 30, 0);
-        if ('comedy' === playFor(aPerformance).type) result += Math.floor(aPerformance.audience / 5);
+        if ('comedy' === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
         return result;
-    }
-
-    function playFor(aPerformance) {
-        return plays[aPerformance.playID];
-    }            
+    }          
 
     function amountFor(aPerformance){
         let result = 0;
         
-        switch (playFor(aPerformance).type) {
+        switch (aPerformance.play.type) {
             case 'tragedy':
                 result = 40000;
                 if (aPerformance.audience > 30) {
