6d8be150d5079b681b9d28eb31cd0cb126d619d4
周丸生(SamChou) <SamChou@chipbond.com.tw>
2020-03-22 14:52:41 +0800

    mv volumeCredits calc function

--- src/bf/ch1ReOrgCalcByType/createStatementData.js
+++ src/bf/ch1ReOrgCalcByType/createStatementData.js
@@ -11,7 +11,7 @@ export default function createStatementData(invoice, plays) {
         const result = Object.assign({}, aPerformance);        
         result.play = calculator.play;
         result.amount = calculator.amount;
-        result.volumeCredits = volumeCreditsFor(result);
+        result.volumeCredits = calculator.volumeCredits;
         return result;
     }
 
@@ -20,6 +20,13 @@ export default function createStatementData(invoice, plays) {
             this.performance = aPerformance;
             this.play = aPlay;
         }  
+
+        get volumeCredits(){
+            let result = 0;
+            result += Math.max(this.performance.audience - 30, 0);
+            if ('comedy' === this.play.type) result += Math.floor(this.performance.audience / 5);
+            return result;
+        }           
 
         get amount(){
             let result = 0;
@@ -58,14 +65,7 @@ export default function createStatementData(invoice, plays) {
     function totalVolumeCredits(data) {
         return data.performances
         .reduce((total, p) => total + p.volumeCredits, 0);
-    }     
-
-    function volumeCreditsFor(aPerformance){
-        let result = 0;
-        result += Math.max(aPerformance.audience - 30, 0);
-        if ('comedy' === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-        return result;
-    }          
+    }            
 
     function playFor(aPerformance) {
         return plays[aPerformance.playID];
