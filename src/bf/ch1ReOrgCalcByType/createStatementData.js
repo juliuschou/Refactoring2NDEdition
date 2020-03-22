df44c7598efa3bbc13d305b37784e9abd44df03f
周丸生(SamChou) <SamChou@chipbond.com.tw>
2020-03-22 14:55:54 +0800

    **making the perf. calc*tor polymorphic**

--- src/bf/ch1ReOrgCalcByType/createStatementData.js
+++ src/bf/ch1ReOrgCalcByType/createStatementData.js
@@ -1,4 +1,5 @@
 export default function createStatementData(invoice, plays) {
+    
     const statementData = {};
     statementData.customer = invoice.customer;
     statementData.performances = invoice.performances.map(enrichPerformance);
