0020c20fa5b826ea90b7dfa2e1d95384f37eea8c
周丸生(SamChou) <SamChou@chipbond.com.tw>
2020-03-22 12:54:53 +0800

    add vol*dits to inter*ate data by mv vol*Cred*For

--- src/af/ch1SplitPhase/StartingPoint.js
+++ src/af/ch1SplitPhase/StartingPoint.js
@@ -38,9 +38,17 @@ function statement(invoice, plays) {
         const result = Object.assign({}, aPerformance);        
         result.play = playFor(result);
         result.amount = amountFor(result);
+        result.volumeCredits = volumeCreditsFor(result);
         return result;
     }
     
+    function volumeCreditsFor(aPerformance){
+        let result = 0;
+        result += Math.max(aPerformance.audience - 30, 0);
+        if ('comedy' === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
+        return result;
+    }          
+
     function playFor(aPerformance) {
         return plays[aPerformance.playID];
     }  
@@ -90,20 +98,13 @@ function renderPlainText(data, plays) {
     function totalVolumeCredits(){
         let result = 0;
         for (let perf of data['performances']) {
-            result += volumeCreditsFor(perf);
+            result += perf.volumeCredits;
         }
         return result;
     }
     
     function usd(aNumber){
         return new Intl.NumberFormat('en-IN', { maximumSignificantDigits: 3 }).format(aNumber);
-    }
-
-    function volumeCreditsFor(aPerformance){
-        let result = 0;
-        result += Math.max(aPerformance.audience - 30, 0);
-        if ('comedy' === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-        return result;
-    }          
+    }       
 
 }
